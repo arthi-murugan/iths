9ee92977cb22146219641dc5f323fdf73ad5a095
Arthilakshmi Murugan <arthilakshmimurugan@gmail.com>
2022-11-27 21:23:59 +0100

    Added more test cases and optimised program.

--- src/calc.py
+++ src/calc.py
@@ -7,17 +7,43 @@ class Calculator(object):
         return self._last_answer
 
     def add(self, a, b):
-        self._last_answer = a + b
+        try:
+            self._last_answer = a + b
+        except TypeError:
+            raise TypeError("Only integers are allowed")
         return self.last_answer
 
     def subtract(self, a, b):
-        self._last_answer = a - b
+        try:
+            self._last_answer = a - b
+        except TypeError:
+            raise TypeError("Only integers are allowed")
         return self.last_answer
 
     def multiply(self, a, b):
-        self._last_answer = a * b
+        try:
+            self._last_answer = a * b
+        except TypeError:
+            raise TypeError("Only integers are allowed")
         return self.last_answer
 
     def divide(self, a, b):
-        self._last_answer = a * 1.0 / b
+        try:
+            self._last_answer = a / b
+        except ZeroDivisionError:
+            raise ZeroDivisionError("Can not divide by 0")
+        return self.last_answer
+
+    def maximum(self, a, b):
+        try:
+            self._last_answer = a if a >= b else b
+        except TypeError:
+            raise TypeError("Only integers are allowed")
+        return self.last_answer
+
+    def minimum(self, a, b):
+        try:
+            self._last_answer = a if a <= b else b
+        except TypeError:
+            raise TypeError("Only integers are allowed")
         return self.last_answer
--- test/test_calc.py
+++ test/test_calc.py
@@ -2,8 +2,10 @@ import unittest
 
 from src.calc import Calculator
 
-NUMBER_1 = 3
-NUMBER_2 = 2
+P_NUMBER_1 = 3
+P_NUMBER_2 = 2
+N_NUMBER_1 = -3
+N_NUMBER_2 = -2
 
 
 class CalculatorTest(unittest.TestCase):
@@ -15,28 +17,73 @@ class CalculatorTest(unittest.TestCase):
         self.assertEqual(value, 0.0)
 
     def test_add(self):
-        value = self.calc.add(NUMBER_1, NUMBER_2)
+        value = self.calc.add(P_NUMBER_1, P_NUMBER_2)
         self.assertEqual(value, 5.0)
         self.assertEqual(value, self.calc.last_answer)
 
-    def test_subtract(self):
-        value = self.calc.subtract(NUMBER_1, NUMBER_2)
-        self.assertEqual(value, 1.0)
+    def test_add_with_negative_number(self):
+        value = self.calc.add(N_NUMBER_1, N_NUMBER_2)
+        self.assertEqual(value, -5.0)
         self.assertEqual(value, self.calc.last_answer)
 
+    def test_add_with_string(self):
+        self.assertRaises(TypeError, self.calc.add, P_NUMBER_1, 'text')
+
+    def test_subtract(self):
+        self.assertRaises(TypeError, self.calc.subtract, P_NUMBER_1, None)
+
     def test_subtract_negative(self):
-        value = self.calc.subtract(NUMBER_2, NUMBER_1)
+        value = self.calc.subtract(P_NUMBER_2, P_NUMBER_1)
         self.assertEqual(value, -1.0)
         self.assertEqual(value, self.calc.last_answer)
 
+    def test_subtract_with_negative(self):
+        value = self.calc.subtract(N_NUMBER_1, N_NUMBER_2)
+        self.assertEqual(value, -1.0)
+        self.assertEqual(value, self.calc.last_answer)
+
+    def test_add_with_string(self):
+        self.assertRaises(TypeError, self.calc.divide, P_NUMBER_1, 'text')
+
     def test_multiply(self):
-        value = self.calc.multiply(NUMBER_1, NUMBER_2)
+        value = self.calc.multiply(P_NUMBER_1, P_NUMBER_2)
         self.assertEqual(value, 6.0)
         self.assertEqual(value, self.calc.last_answer)
 
     def test_divide(self):
-        value = self.calc.divide(NUMBER_1, NUMBER_2)
+        value = self.calc.divide(P_NUMBER_1, P_NUMBER_2)
         self.assertEqual(value, 1.5)
         self.assertEqual(value, self.calc.last_answer)
 
+    def test_divide_by_zero(self):
+        self.assertRaises(ZeroDivisionError, self.calc.divide, P_NUMBER_1, 0)
 
+    def test_max_greater(self):
+        value = self.calc.maximum(P_NUMBER_1, P_NUMBER_2)
+        self.assertEqual(value, P_NUMBER_1)
+        self.assertEqual(value, self.calc.last_answer)
+
+    def test_max_less(self):
+        value = self.calc.maximum(P_NUMBER_2, P_NUMBER_1)
+        self.assertEqual(value, P_NUMBER_1)
+        self.assertEqual(value, self.calc.last_answer)
+
+    def test_max_equal(self):
+        value = self.calc.maximum(P_NUMBER_1, P_NUMBER_1)
+        self.assertEqual(value, P_NUMBER_1)
+        self.assertEqual(value, self.calc.last_answer)
+
+    def test_min_greater(self):
+        value = self.calc.minimum(P_NUMBER_1, P_NUMBER_2)
+        self.assertEqual(value, P_NUMBER_2)
+        self.assertEqual(value, self.calc.last_answer)
+
+    def test_min_less(self):
+        value = self.calc.minimum(P_NUMBER_2, P_NUMBER_1)
+        self.assertEqual(value, P_NUMBER_2)
+        self.assertEqual(value, self.calc.last_answer)
+
+    def test_min_equal(self):
+        value = self.calc.minimum(P_NUMBER_2, P_NUMBER_2)
+        self.assertEqual(value, P_NUMBER_2)
+        self.assertEqual(value, self.calc.last_answer)
